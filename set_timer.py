bdc93965bc98b73cc66e4259fddbc7f87cdb7674
pmeiha <ha@pmei.ch>
2024-03-26 22:22:16 +0100

    form button

--- set_timer.py
+++ set_timer.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from pprint import pprint
+from markupsafe import escape
 import requests
 import os
 
@@ -12,19 +13,50 @@ def get_timer(dev_ip):
     return timer_data
 
 
+def line_form(line_nr, edit=True, delete=True, insert=True, etext="edit", dtext="delete", itext="insert oben"):
+
+    t_out = ""
+
+    if edit or delete or insert:
+        t_out += f'<form action="/edit_line"><input type="hidden" value="{
+            escape(line_nr)}">'
+
+        if edit:
+            t_out += f'<button type="submit" name="edit">{
+                escape(etext)}</button>'
+
+        if delete:
+            t_out += f'<button type="submit" id="delete">{
+                escape(dtext)}</button>'
+
+        if insert:
+            t_out += f'<button type="submit" id="insert">{
+                escape(itext)}</button>'
+
+        t_out += '</form>'
+    return t_out
+
+
 def get_table(text_in):
 
-    text_out = "<tr><th>line_nr</th><th>command</th></tr>"
+    text_out = "<tr><th>1</th><th>2</th><th>3</th><th>4</th></tr>"
     line_nr = 1
     for line in text_in.splitlines():
 
+        hline = ""
+
         if line.find('addClockEvent') == 0:
-            hline = f"<tr><td>{line_nr}</td><td>{line}</td></tr>"
+            sline = line.split(" ")
+            hline = f"<tr><td>{
+                line_form(line_nr)}</td><td>{escape(sline[1])}</td><td>{escape(sline[2])}</td><td>{escape(sline[5])}</td></tr>"
             line_nr += 1
-        else:
-            hline = f"<tr><td></td><td>{line}</td></tr>"
+        # else:
+        #     hline = f"<tr><td></td><td>{escape(line)}</td></tr>"
 
         text_out += hline
+
+    text_out += f"<tr><td>{line_form(line_nr, edit=False, delete=False, itext="insert new")
+                           }</td><td>2</td><td>3</td><td>4</td></tr>"
 
     return text_out
 
